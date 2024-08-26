c27ac26dde38af1833805bb720baf7dbf30f6e13
Vaibhav Rohaan <82140153+vaibhxv@users.noreply.github.com>
2024-08-26 20:32:30 +0530

    Cleaned some comments

--- api/routes/UserRoutes.js
+++ api/routes/UserRoutes.js
@@ -1,10 +1,9 @@
-// routes/userRoutes.js
 const express = require('express');
 const { getUsers, getUserById, addUser, deleteUser } = require('../controllers/UserController');
 
 const router = express.Router();
 
-// Routes for user management
+
 router.get('/users', getUsers);
 router.get('/users/:id', getUserById);
 router.post('/users', addUser);
